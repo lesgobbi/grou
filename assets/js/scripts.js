13beec885762ab546668b1dd4149d46b0f08241f
Jean Reis <jeanpreis@gmail.com>
2019-10-03 17:53:40 -0300

    frescurinhas home

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
+    AOS.init();
+
     var top = $('.parallax').offset().top;
     var scroll = $(window).scrollTop();
     var height = $('.parallax').height();
     $('.parallax').css('background-position', '0px -' + ((height + top - scroll) / 5) + 'px');
+
+    $('.contato').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $('footer').offset().top
+        }, 1000);
+    });
+
+    if (30 >= scroll) {
+        $('header').removeClass('scrolled');
+    } else {
+        $('header').addClass('scrolled');
+    }
 });
 
 $(window).scroll(function() {
@@ -10,4 +24,10 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     var height = $('.parallax').height();
     $('.parallax').css('background-position', '0px -' + ((height + top - scroll) / 5) + 'px');
+
+    if (30 >= scroll) {
+        $('header').removeClass('scrolled');
+    } else {
+        $('header').addClass('scrolled');
+    }
 });
